c35bc1519e7e6843d504cd1ac39d2c21a849c4e2
Maksym Palamarchuk <97300407+Maks-Palamar@users.noreply.github.com>
2024-01-30 02:54:02 +0200

    upd functions done

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,14 +1,48 @@
 const KEY = "feedback-form-state";
 
-const form = document.querySelector('feedback-form');
-inpSelector.addEventListener("input", inputListenFunc);
+const form = document.querySelector('.feedback-form');
+form.addEventListener("input", inputListenFunc);
 
 function inputListenFunc(event) {
-    const name = form.elements.name.value;
+    const email = form.elements.email.value;
     const message = form.elements.message.value;
 
     const userData = {
-        name: name,
+        email: email,
         message: message,
     }
+
+saveToLS(KEY, userData);
 }
+
+function saveToLS(key, value) {
+    const jsonData = JSON.stringify(value);
+    localStorage.setItem(key, jsonData);
+}
+
+function loadFromLS(key = 'mustbeakey') {
+    const gotKey = localStorage.getItem(key);
+    try {
+      const result = JSON.parse(gotKey);
+      return result;
+    } catch {
+      return gotKey;
+    }
+  }
+  
+form.addEventListener("submit", sendData);
+
+function sendData(event) {
+    event.preventDefault();
+    loadFromLS(KEY);
+    localStorage.removeItem(KEY);
+    form.reset();
+}
+
+function inputData() {
+    const gotData = loadFromLS(KEY) || {};
+    form.elements.email.value = gotData.email || "@";
+    form.elements.message.value = gotData.message || "";
+}
+
+inputData();
